680f828a66fee4fbfebddfe44637c79701d4a9c7
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-06-09 22:44:59 +0600

    Add files via upload
    lab 1

---
+++ Lab1/SQLQuery1.sql
@@ -0,0 +1,19 @@
+Create Database Test
+Alter Database Lab1 Modify Name = Lab2
+Create Table Employee (
+Id int Not Null, name nvarchar (50) Not Null,
+
+)
+Insert Into Employee(Id,name)
+Values (11, 'Shorna')
+
+Insert Into Employee(Id,name)
+Values (12, 'Siam')
+
+Select * from Employee
+Select * from Student
+
+Drop Table Employee
+
+Truncate Table Student
+
